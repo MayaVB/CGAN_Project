15de936d70c5234855418894f7b933cb54740b0f
MayaV <mayaveismman@gmail.com>
2023-03-05 20:50:31 +0200

    updated data bases in git + read paths (so it wont be red from local directory)

--- main.py
+++ main.py
@@ -59,10 +59,16 @@ if __name__ == "__main__":
     #     shuffle=True,
     # )
 
-    mixup_dataset = MnistMixup(r"/home/aharrar/PycharmProjects/Dataset_torch/mnist", to_mix=True, size=10000,
-                               transform=True)
-    normal_dataset = MnistMixup(r"/home/aharrar/PycharmProjects/Dataset_torch/mnist", to_mix=False, size=10000,
-                                transform=True)
+    #mixup_dataset = MnistMixup(r"/home/aharrar/PycharmProjects/Dataset_torch/mnist", to_mix=True, size=10000,
+    #                           transform=True)
+    #normal_dataset = MnistMixup(r"/home/aharrar/PycharmProjects/Dataset_torch/mnist", to_mix=False, size=10000,
+    #                            transform=True)
+
+    mixup_dataset = MnistMixup('.\\data\\MNIST', to_mix=True, size=10000, transform=True)
+    normal_dataset = MnistMixup('.\\data\\MNIST', to_mix=False, size=10000, transform=True)
+
+
+
     print(len(mixup_dataset))
     datasets = [normal_dataset, mixup_dataset]
     datasets = ConcatDataset(datasets)
