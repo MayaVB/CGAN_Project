190dd4a3ddbf0e2a1437091c104dc5237d2c5b60
MayaVB <mayaveismman@gmail.com>
2023-05-02 01:28:47 +0300

    1) minus on loss value
    2) save FID score in list

--- train.py
+++ train.py
@@ -121,12 +121,12 @@ def train(n_epochs, n_classes, latent_dim, dataloader, generator, discriminator
                   % (epoch, n_epochs, i, len(dataloader), d_loss.item(), g_loss.item()))
 
             # d_loss_list.append(d_loss.detach().numpy())
-            d_loss_agg += d_loss.item()
+            d_loss_agg += -d_loss.item()
 
             # g_loss_list.append(g_loss.detach().numpy())
-            g_loss_agg += g_loss.item()
+            g_loss_agg += -g_loss.item()
 
-            eval(generator, imgs, save_images_path, n_classes, latent_dim, franchest)
+            fid_score = eval(generator, imgs, save_images_path, n_classes, latent_dim, franchest)
 
         d_loss_agg /= i
         d_loss_list.append(d_loss_agg)
@@ -135,9 +135,9 @@ def train(n_epochs, n_classes, latent_dim, dataloader, generator, discriminator
         g_loss_list.append(g_loss_agg)
 
         # FID calculation
-        fid_score = calculate_fid_score_epoch(generated_images, real_images)
+        # fid_score = calculate_fid_score_epoch(generated_images, real_images)
         # print("FID score: ", fid_score)
-        fid_score_list.append(fid_score)
+        fid_score_list.append(fid_score.item())
 
         # save loss
 
@@ -179,7 +179,9 @@ def eval(generator, images_real, save_images_path="", n_classes=10, latent_dim=1
 
     gen_imgs = generator(z, labels)
     score = franchest.compute_fid(gen_imgs, images_real)
-    print(score)
+    #print(score)
+
+    return score
 
     # save_image(gen_imgs.data, save_images_path, nrow=n_classes, normalize=True)
 
